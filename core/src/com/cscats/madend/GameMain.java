ecae4e576464c1d8ef8d0142b766e5432693a458
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2021-12-06 23:21:04 +0300

    Background deleted

--- core/src/com/cscats/madend/GameMain.java
+++ core/src/com/cscats/madend/GameMain.java
@@ -12,7 +12,7 @@ public class GameMain extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("");
 	}
 
 	@Override
