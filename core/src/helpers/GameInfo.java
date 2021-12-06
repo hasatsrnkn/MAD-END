1148ce4d139bc431417d3a77501bf723d3941c13
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2021-12-06 23:28:09 +0300

    Game Info added

---
+++ core/src/helpers/GameInfo.java
@@ -0,0 +1,15 @@
+package helpers;
+
+/**
+ * Game width, height, pixel per meter
+ * @author Mehmet Hasat Serinkan
+ * @date 06.12.2021
+ */
+public class GameInfo {
+
+    public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
+    public static final int PPM = 100;
+
+
+}
