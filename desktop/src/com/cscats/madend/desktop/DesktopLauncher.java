906bc0ceda271583302359e743c3394b707184e7
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2021-12-06 23:30:42 +0300

    GameMain width and height changed

--- desktop/src/com/cscats/madend/desktop/DesktopLauncher.java
+++ desktop/src/com/cscats/madend/desktop/DesktopLauncher.java
@@ -3,11 +3,13 @@ package com.cscats.madend.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.cscats.madend.GameMain;
+import helpers.GameInfo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new GameMain(), config);
-		config.height = 1000;
+		config.height = GameInfo.HEIGHT;
+		config.width = GameInfo.WIDTH;
 	}
 }
